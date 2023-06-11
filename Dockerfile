d62d62afbf473b15194f9e0063af288ce0c6928b
robertwadelek <60813152+robertwadelek@users.noreply.github.com>
2023-06-11 19:08:15 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 5004
 
 ENTRYPOINT ["python"]
 
--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def get_prediction():
     return jsonify(features=features, predicted_class=predicted_class)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5004)
